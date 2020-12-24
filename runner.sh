5ff4fc82fa580ced50b9fb79d8f7a253bc183c40
ayoubSamil <asamil@pimarketing.com>
2020-12-24 13:23:10 +0100

    remove prepare function

--- runner.sh
+++ runner.sh
@@ -7,14 +7,9 @@
 
 function main() {
     os_name=$(get_system)
-    prepare "$os_name"
     install_mysql "$os_name"
     install_postfix "$os_name"
     install_ip_table "$os_name"
     }
-
-function prepare() {
-    install "dos2unix" "$os_name" ("dos2unix") && dos2unix *.sh
-}
 main
 exit
